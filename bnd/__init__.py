07f3622a8353b9e7948fa85768911805d4df547a
Mostafa Safaie <mostafa.safaie@gmail.com>
2025-04-24 16:26:10 +0100

    expose `init`

--- bnd/__init__.py
+++ bnd/__init__.py
@@ -1,6 +1,6 @@
 # Package interface
-from .cli import dl, up, to_pyal, batch_ks
+from .cli import dl, up, to_pyal, batch_ks, init
 from .data_transfer import upload_session, download_session, download_animal
 
-__all__ = [dl, up, to_pyal, batch_ks, upload_session, download_session, download_animal]
+__all__ = [dl, up, to_pyal, batch_ks, upload_session, download_session, download_animal, init]
 
