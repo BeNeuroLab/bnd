977d427c83589b072ea61e78a1e597f7ffa1d5a2
Mostafa Safaie <mostafa.safaie@gmail.com>
2025-05-27 13:37:22 +0100

    fix __all__

--- bnd/__init__.py
+++ bnd/__init__.py
@@ -2,5 +2,5 @@
 from .cli import dl, up, to_pyal, batch_ks, init
 from .data_transfer import upload_session, download_session, download_animal
 
-__all__ = [dl, up, to_pyal, batch_ks, upload_session, download_session, download_animal, init]
+__all__ = ["dl", "up", "to_pyal", "batch_ks", "upload_session", "download_session", "download_animal", "init"]
 
